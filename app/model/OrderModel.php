427c919884d6e3ef3332366c9fbc5cfe088528c6
Kifa <Kifa@Thinki>
2013-03-19 10:42:06 +0100

    Upgraded OrderModel - added function

--- app/model/OrderModel.php
+++ app/model/OrderModel.php
@@ -36,7 +36,7 @@ class OrderModel extends Authenticator {
     public function insertOrder($id, $status, $user, $price, $pricetax, $created, 
             $lastchange, $finished, $delivery, $payment)
     {
-            $insert=  array(
+            $insert =  array(
                 'OrderID' => $id,
                 'StatusID' => $status,
                 'UserID' => $user,
@@ -54,6 +54,9 @@ class OrderModel extends Authenticator {
             
     }
     
+    /*
+     * Insert order details
+     */
     public function insertOrderDetails($id, $orderid, $product, $quantity, $unitprice) 
     {
         $insert = array(
@@ -65,13 +68,99 @@ class OrderModel extends Authenticator {
         );    
         return $this->getTable('orderdetails')->insert($insert);
     }
+    
+    /*
+     * Load order statuses
+     */
+    public function loadOrderStatus($id)
+    {
+        if($id==''){
+            return $this->getTable('orderstatus');
+        }
+        else
+        {
+            return $this->getTable('orderstatus')->where('StatusID',$id);
+        }
+    }
+    
+    /*
+     * Insert new order status
+     */
+    public function insertOrderStatus($id,$name,$description)
+    {
+        $insert = array(
+            'OrderStatusID' => $id,
+            'OrderStatusName' => $name,
+            'OrderStatusDescription' => $description
+        );
+        
+        return $this->getTable('orderstatus')->insert($insert);
+    }
+
+    /*
+     * Load payment method for order
+     */
+    public function loadPaymentMethod($id){
+        if($id==''){
+            return $this->getTable('paymentmethod');
+        }
+        else
+        {
+            return $this->getTable('paymentmethod')->select('paymentmethod.*,price.finalprice')
+                    ->where('PaymentMethodID',$id);
+        }
+    }
+    
+    /*
+     * Insert new payment method
+     */
+    public function insertPaymentMethod($id,$name,$price)
+    {
+        $insert = array(
+            'PaymentMethodID' => $id,
+            'PaymentMethodName' => $name,
+            'PaymentPriceID' => $price
+        );
+                
+        return $this->getTable('paymentmethod')->insert($insert);
+    }
+    
+    /*
+     * Load delivery method
+     */
+    public function loadDelivery($id)
+    {
+        if($id==''){
+            return $this->getTable('delivery');
+        }
+        else
+        {
+            return $this->getTable('delivery')->select('delivery.*,price.FinalPrice')->where('DeliveryID',$id);
+        }
+    }
+    
+    /*
+     * Insert new delivery method
+     */
+    public function insertDelivery($id,$type,$description,$price,$free)
+    {
+        $insert = array(
+            'DeliveryID' => $id,
+            'TypeOfDelivery' => $type,
+            'DeliveryDescription' => $description,
+            'PriceID' => $price,
+            'FreeFromPrice' => $free
+        );
+        
+        return $this->getTable('delivery')->insert($insert);
+    }
     /*
      * Change order status
      * @param ?
      * @param ? example: pozice počátečního znaku
      * @return string
      */
-
+    
 
     /*
      * Generate emails
