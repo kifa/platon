bfe55259a34fc9ced1e0d503eaf219d738cc3a18
Kifa <Kifa@Thinki>
2013-03-28 17:15:27 +0100

    Insert User & Address

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -20,14 +20,39 @@ class UserModel extends Repository{
         // $userId = $this->userPocet();
 
         $this->getTable('users')->insert(array(
-                                            'Login' => $username,
+                                            'Email' => $username,
                                             'Password' => Authenticator::calculateHash($password),                                            
-                                            'FirstName' => $name));
+                                            'Name' => $name));
     } 
     
      public function setPassword($username, $password) {
        $this->getTable('users')->where('Login', $username)->update(array
             ('Password' => Authenticator::calculateHash($password)));
     }
+    
+    public function insertUser($email,$name,$phone,$address){
+        $insert = array(
+                'Email' => $email,
+                'Name' => $name,
+                'PhoneNumber' => $phone,
+                'AddressID' => $address
+                );
+        
+        return $this->getTable('user')->insert($insert);
+    }
+    
+    public function insertAddress($id,$street,$city,$zip){
+        $insert = array(
+            'AddressID' => $id,
+            'Street' => $street,
+            'ZIPCode' => $zip,
+            'City' => $city
+        );
+        
+        return $this->getTable('address')->insert($insert);
+    }
 
+    public function countAddress(){
+        return $this->getTable('address')->count();
+    }
 }
