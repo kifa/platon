13cbe4c592d6587959cb837bbccb4779812f59a0
Kifa <Kifa@Thinki>
2013-05-24 10:27:19 +0200

    OrderModel update

--- app/model/BlogModel.php
+++ app/model/BlogModel.php
@@ -12,24 +12,29 @@
  */
 class BlogModel extends Repository {
     
-    public function loadCategoryList(){
-        return $this->getTable('Category')->where('CategoryStatus', $id)->fetchPairs('CategoryID');
+    public function loadCategoryList($id='1'){
+        return $this->getTable('category')->where('CategoryStatus', $id)->fetchPairs('CategoryID');
     }
     
     public function loadCategory($id){
-        return $this->getTable('Category')->where('CategoryID', $id)->fetch();
+        return $this->getTable('category')->where('CategoryID', $id)->fetch();
     }
     
     public function loadPosts($id) {
         
         if($id == NULL) {
-        return $this->getTable('Blog')->fetchPairs('BlogID');
+        return $this->getTable('blog')->fetchPairs('BlogID');
         }
         else {
-        return $this->getTable('Blog')->where('CategoryID', $id)->fetchPairs('BlogID');    
+        return $this->getTable('blog')->where('CategoryID', $id)->fetchPairs('BlogID');    
         }
     }
     
+    public function loadPost($id){
+        return $this->getTable('blog')->where('BlogID',$id)->fetch();
+        
+    }
+
     public function insertPost($name, $content, $categoryID) {
 
         $insert = array(
@@ -38,21 +43,21 @@ class BlogModel extends Repository {
           'BlogCategory' => $categoryID
         );
 
-        return $this->getTable('Blog')->insert($insert);
+        return $this->getTable('blog')->insert($insert);
     }
     
     public function updatePost($id, $name, $content, $categoryID) {
-        $insert = array(
+        $update = array(
           'BlogName' => $name,
           'BlogContent' => $content,
           'BlogCategory' => $categoryID
         );
 
-        return $this->getTable('Blog')->where('BlogID', $id)->insert($insert);
+        return $this->getTable('blog')->where('BlogID', $id)->update($update);
     }
 
     public function deletePost($id) {
-        return $this->getTable('Blog')->where('BlogID', $id)->delete();
+        return $this->getTable('blog')->where('BlogID', $id)->delete();
     }
     
     
