c05c0e2ec1c8071466e93b5f4d7da815f7c6d695
Kifa <Kifa@Thinki>
2013-08-24 12:16:14 +0200

    Search v jednom dotazu

--- app/model/BlogModel.php
+++ app/model/BlogModel.php
@@ -8,124 +8,110 @@
 class BlogModel extends Repository {
     
     public function loadCategoryList($id='1'){
-        return $this->getTable('category')->where('CategoryStatus', $id)->fetchPairs('CategoryID');
+        return $this->getTable('category')
+                ->where('CategoryStatus', $id)
+                ->fetchPairs('CategoryID');
     }
     
     public function loadCategory($id = NULL){
         if($id == NULL) {
-            return $this->getTable('category')->where('CategoryID', 99)->fetch();
+            return $this->getTable('category')
+                    ->where('CategoryID', 99)
+                    ->fetch();
         }
         else {
-            return $this->getTable('category')->where('CategoryID', $id)->fetch();
+            return $this->getTable('category')
+                    ->where('CategoryID', $id)
+                    ->fetch();
         }
     }
     
     public function loadPosts($id = NULL) {
         
         if($id == NULL) {
-            // return $this->getTable('blog')->order('BlogID DESC')->fetchPairs('BlogID');   
-           return $this->getTable('blog')->order('BlogID DESC')->fetchPairs('BlogID');           
-            
-           /* return $this->getDB()->query('
-                SELECT * FROM blog 
-                JOIN photoalbum ON blog.BlogID=photoalbum.BlogID 
-                JOIN photo ON photoalbum.PhotoAlbumID=photo.PhotoAlbumID
-                ORDER BY blog.BlogID DESC');            */
-            
+           return $this->getTable('blog')
+                   ->order('BlogID DESC')
+                   ->fetchPairs('BlogID');                       
         }
         else {
-            return $this->getTable('blog')->where('CategoryID', $id)->order('BlogID DESC')->fetchPairs('BlogID');    
-            /*return $this->getDB()->query('SELECT * FROM blog JOIN photoalbum ON blog.BlogID=photoalbum.BlogID 
-            JOIN photo ON photoalbum.PhotoAlbumID=photo.PhotoAlbumID 
-             WHERE Photo.CoverPhoto="1" blog.CategoryID=?',$id);  */
+            return $this->getTable('blog')
+                    ->where('CategoryID', $id)
+                    ->order('BlogID DESC')
+                    ->fetchPairs('BlogID');    
         }
     }
     
     public function loadPost($postid){
-        return $this->getTable('blog')->where('BlogID',$postid)->fetch();        
-        //$return = $this->getTable('blog')->where('BlogID',$postid)->fetch();        
-        //$returnX = array(
-        //    'BlogID' => $return['BlogID'],
-        //    'BlogName' => $return['BlogName'],
-        //    'PhotoAlbumID' => '0',
-        //    'BlogContent' => $return['BlogContent'],
-        //    'CategoryID' => NULL
-        //);
-                    
-        //return $returnX;
-        
-      /*  return $this->getDB()->query('
-                SELECT * FROM blog
-                JOIN photoalbum ON blog.BlogID=photoalbum.BlogID 
-                JOIN photo ON photoalbum.PhotoAlbumID=photo.PhotoAlbumID
-                WHERE blog.BlogID=?',$postid)->fetch();  
-        */
+        return $this->getTable('blog')
+                ->where('BlogID', $postid)
+                ->fetch();        
     }
 
-    public function insertPost($name,  $categoryID, $content) {
+    public function insertPost($name, $categoryID, $content, $product = NULL) {
 
         $insert = array(
-          'BlogName' => $name,
-         // 'BlogDescription' => $desc,
-          'BlogContent' => $content,
-          'CategoryID' => $categoryID
+            'ProductID' => $product,
+            'BlogName' => $name,
+            'BlogContent' => $content,
+            'CategoryID' => $categoryID
         );
-
         
-        $return = $this->getTable('blog')->insert($insert);
+        $return = $this->getTable('blog')
+                ->insert($insert);
+        
         return $return->BlogID;
     }
     
-
     public function updatePost($id, $where, $content) {
         $update = array(
           $where => $content,
         );        
-        return $this->getTable('blog')->where('BlogID', $id)->update($update);
+        
+        return $this->getTable('blog')
+                ->where('BlogID', $id)
+                ->update($update);
     }
 
     public function deletePost($id) {
-        return $this->getTable('blog')->where('BlogID', $id)->delete();
+        return $this->getTable('blog')
+                ->where('BlogID', $id)
+                ->delete();
     }
     
     public function loadCoverPhoto($id){        
-        $photo = $this->getTable('photo')->select('photo.PhotoURL, photoalbum.BlogID')->where('photoalbum.BlogID',$id)
-                ->where('photo.CoverPhoto','1')->fetch();
+        $photo = $this->getTable('photo')
+                ->select('photo.PhotoURL, photoalbum.BlogID')
+                ->where('photoalbum.BlogID',$id)
+                ->where('photo.CoverPhoto','1')
+                ->fetch();
+        
         if($photo["PhotoURL"]==""){
             $photo["PhotoURL"]=1;
         }
+        
         return $photo["PhotoURL"];
     }
-    
-    
-     public function loadPhotoAlbum($id){
+        
+    public function loadPhotoAlbum($id){
         if($id==''){
             return $this->getTable('PhotoAlbum');
         }
         else{
-            //return $this->getTable('PhotoAlbum')->where('ProductID',$id);
-            return $this->getTable('photo')->select('photo.*,photoalbum.*')->where('photoalbum.blogID',$id)->fetchPairs('PhotoID');
-            /*$row = $this->getDB()->query('
-                SELECT * FROM blog 
-                JOIN photoalbum ON blog.BlogID=photoalbum.BlogID 
-                JOIN photo ON photoalbum.PhotoAlbumID=photo.PhotoAlbumID 
-                WHERE Blog.BlogID=?',$id);*/           
-        }                
-                        
+            return $this->getTable('photo')
+                    ->select('photo.*,photoalbum.*')
+                    ->where('photoalbum.blogID',$id)
+                    ->fetchPairs('PhotoID');            
+        }                                        
     }
  
     public function loadPhotoAlbumID($postid){
         if($postid==''){
             $photoalbum = $this->getTable('photoalbum');
         }
-        else{
-            //return $this->getTable('PhotoAlbum')->where('ProductID',$id);
-            $photoalbum = $this->getTable('photoalbum')->where('photoalbum.blogID',$postid)->fetch();
-            /*$row = $this->getDB()->query('
-                SELECT * FROM blog 
-                JOIN photoalbum ON blog.BlogID=photoalbum.BlogID 
-                JOIN photo ON photoalbum.PhotoAlbumID=photo.PhotoAlbumID 
-                WHERE Blog.BlogID=?',$id);*/           
+        else{            
+            $photoalbum = $this->getTable('photoalbum')
+                    ->where('photoalbum.blogID',$postid)
+                    ->fetch();            
         }
         
         if($photoalbum == FALSE){
@@ -142,13 +128,10 @@ class BlogModel extends Repository {
             return $this->getTable('photoalbum');
         }
         else{
-            //return $this->getTable('PhotoAlbum')->where('ProductID',$id);
-            return $this->getTable('photo')->select('photo.*,photoalbum.*')->where('photoalbum.StaticTextID',$postid)->fetchPairs('PhotoID');
-            /*$row = $this->getDB()->query('
-                SELECT * FROM blog 
-                JOIN photoalbum ON blog.BlogID=photoalbum.BlogID 
-                JOIN photo ON photoalbum.PhotoAlbumID=photo.PhotoAlbumID 
-                WHERE Blog.BlogID=?',$id);*/           
+            return $this->getTable('photo')
+                    ->select('photo.*,photoalbum.*')
+                    ->where('photoalbum.StaticTextID', $postid)
+                    ->fetchPairs('PhotoID');            
         }
     }
     
